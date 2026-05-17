006d218960e42a292e623ea0e9bd433b8a1253a1
Goutham K <kgoutham13209@gmail.com>
2026-05-17 15:56:38 +0000

    daily_weather model created

---
+++ models/daily_weather.sql
@@ -0,0 +1,35 @@
+
+WITH daily_weather AS(
+
+SELECT 
+DATE(TIME) AS daily_weather,
+weather,
+temp,
+pressure,
+humidity,
+clouds
+ 
+FROM {{ source('demo', 'weather') }}
+
+LIMIT 10
+
+),
+
+daily_weather_agg as 
+
+(
+select daily_weather,
+weather,
+ROUND(AVG(temp),3) AS AVG_TEMP,
+ROUND(AVG(pressure),3) AS AVG_PRESSURE,
+ROUND(AVG(humidity),3) AS AVG_HUMIDITY,
+ROUND(AVG(clouds),3) AS AVG_CLOUDS
+
+FROM daily_weather
+GROUP BY daily_weather,weather
+QUALIFY  ROW_NUMBER() OVER(PARTITION BY daily_weather ORDER BY COUNT(weather) DESC) =1
+)
+
+SELECT 
+* 
+FROM daily_weather_agg
